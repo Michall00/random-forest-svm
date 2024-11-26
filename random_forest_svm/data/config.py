f000a8c6c899597a5247c0b9ea4867f902d3c6a6
Matthev00 <mateuszostaszewskififa@gmail.com>
2024-11-26 14:11:45 +0100

    feat: add dataset configuration and download functionality closes #3

---
+++ random_forest_svm/data/config.py
@@ -0,0 +1,11 @@
+IRIS_ID = 53
+WINE_QUALITY_ID = 186
+CHURN_ID = 563
+
+IRIS_RAW_PATH = 'data/raw/iris.csv'
+WINE_QUALITY_RAW_PATH = 'data/raw/winequality.csv'
+CHURN_RAW_PATH = 'data/raw/churn.csv'
+
+IRIS_NORM_PATH = 'data/processed/iris.csv'
+WINE_QUALITY_NORM_PATH = 'data/processed/winequality.csv'
+CHURN_NORM_PATH = 'data/processed/churn.csv'
