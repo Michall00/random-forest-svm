b4ac84550d03042e35670f9c56df1b57ae98c70f
Matthev00 <mateuszostaszewskififa@gmail.com>
2024-11-26 18:46:09 +0100

    feat: add data loading functions closes #4

---
+++ random_forest_svm/data/load_data.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+
+def load_data(
+    dataset_name: str, column_to_drop: str
+) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Load the dataset and return the data in format (X_svm, y_svm, X_id3, y_id3).
+    """
+    svm_df = pd.read_csv(f"data/processed/{dataset_name}/SVM.csv")
+    id3_df = pd.read_csv(f"data/processed/{dataset_name}/ID3.csv")
+
+    X_svm = svm_df.drop(column_to_drop, axis=1)
+    y_svm = svm_df[column_to_drop]
+    X_id3 = id3_df.drop(column_to_drop, axis=1)
+    y_id3 = id3_df[column_to_drop]
+
+    return X_svm.to_numpy(), y_svm.to_numpy(), X_id3.to_numpy(), y_id3.to_numpy()
+
+
+def load_iris() -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    return load_data("iris", "class")
+
+
+def load_wine_quality() -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    return load_data("wine_quality", "quality")
+
+
+def load_churn() -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    return load_data("churn", "Churn")
